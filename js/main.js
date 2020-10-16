5935075892436f7e564de57cc3a851f8fa036431
FurmanT <silence19@mail.ru>
2020-10-16 16:05:21 +0500

    10. Личный проект: доверяй, но проверяй (часть 1)

--- js/main.js
+++ js/main.js
@@ -11,6 +11,19 @@ const Y_INIT = 130;
 const Y_HEIGTH = 630;
 const WIDTH = 50;
 const HEIGTH = 70;
+const MAIN_PIN_WIDTH = 65;
+const MAIN_PIN_HEIGTH = 65;
+const adForm = document.querySelector('.ad-form');
+const filterForm = document.querySelector('.map__filters');
+const elementFieldsetsAdForm = adForm.querySelectorAll("fieldset");
+const elementSelectsFilterForm = filterForm.querySelectorAll("select");
+const fieldsetFilterForm = filterForm.querySelectorAll("fieldset");
+const blockMap = document.querySelector(".map");
+const addressField = adForm.querySelector("#address");
+const fieldRoom = adForm.querySelector('#room_number');
+const fieldCapacity = adForm.querySelector('#capacity');
+const mapListElement = blockMap.querySelector(`.map__pins`);
+const mainPin = mapListElement.querySelector('.map__pin--main');
 
 const getRandomBetween = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -39,36 +52,32 @@ const createArrayAdvertising = function () {
   let array = [];
   for (let i = 1; i <= COUNT; i++) {
     array.push(
-        {
-          "author": {
-            "avatar": `img/avatars/user0${i}.png`
-          },
-          "offer": {
-            "title": "Заголовок предложения",
-            "address": "600, 350",
-            "price": 50,
-            "type": TYPE[getRandomNumber(TYPE.length)],
-            "rooms": 5,
-            "guests": 5,
-            "checkin": CHECK_TIME[getRandomNumber(CHECK_TIME.length)],
-            "checkout": CHECK_TIME[getRandomNumber(CHECK_TIME.length)],
-            "features": getRandomArray(FEATURES),
-            "description": "Описание",
-            "photos": getRandomArray(IMAGES),
-          },
-          "location": {
-            "x": getRandomBetween(0, blockMap.offsetWidth),
-            "y": getRandomBetween(Y_INIT, Y_HEIGTH),
-          }
-        });
+      {
+        "author": {
+          "avatar": `img/avatars/user0${i}.png`
+        },
+        "offer": {
+          "title": "Заголовок предложения",
+          "address": "600, 350",
+          "price": 50,
+          "type": TYPE[getRandomNumber(TYPE.length)],
+          "rooms": 5,
+          "guests": 5,
+          "checkin": CHECK_TIME[getRandomNumber(CHECK_TIME.length)],
+          "checkout": CHECK_TIME[getRandomNumber(CHECK_TIME.length)],
+          "features": getRandomArray(FEATURES),
+          "description": "Описание",
+          "photos": getRandomArray(IMAGES),
+        },
+        "location": {
+          "x": getRandomBetween(0, blockMap.offsetWidth),
+          "y": getRandomBetween(Y_INIT, Y_HEIGTH),
+        }
+      });
   }
   return array;
 };
 
-let blockMap = document.querySelector(".map");
-blockMap.classList.remove('map--faded');
-
-let mapListElement = blockMap.querySelector(`.map__pins`);
 const advertisingTemplate = document.querySelector(`#pin`)
   .content
   .querySelector(`.map__pin`);
@@ -78,7 +87,7 @@ const renderAdvertising = function (advertising) {
   const img = advertisingElement.querySelector(`img`);
   img.alt = advertising.offer.title;
   img.src = advertising.author.avatar;
-  advertisingElement.style = `left: ${advertising.location.x - (WIDTH / 2) }px; top: ${advertising.location.y - HEIGTH}px;`;
+  advertisingElement.style = `left: ${advertising.location.x + (WIDTH / 2)}px; top: ${advertising.location.y + HEIGTH}px;`;
   return advertisingElement;
 };
 
@@ -91,4 +100,76 @@ const createFragment = function () {
   return fragment;
 };
 
-mapListElement.appendChild(createFragment());
+const setActiveMap = function () {
+  mapListElement.appendChild(createFragment());
+};
+
+
+const setMoveAddressFieldAd = function () {
+  let x = parseInt(mainPin.style.left) + MAIN_PIN_WIDTH / 2;
+  let y = parseInt(mainPin.style.top) + MAIN_PIN_HEIGTH;
+  addressField.value = Math.floor(x) + ',' + Math.floor(y);
+};
+
+const setInitAddressFieldAd = function () {
+  let x = parseInt(mainPin.style.left) + MAIN_PIN_WIDTH / 2;
+  let y = parseInt(mainPin.style.top) + MAIN_PIN_HEIGTH / 2;
+  addressField.value = Math.floor(x) + ',' + Math.floor(y);
+};
+
+const setDisabledState = function () {
+  elementFieldsetsAdForm.forEach(function (element) {
+    element.disabled = true;
+  });
+
+  elementSelectsFilterForm.forEach(function (element) {
+    element.disabled = true;
+  });
+  fieldsetFilterForm.disabled = true;
+  setInitAddressFieldAd();
+};
+
+const setActiveState = function () {
+  let active = false;
+  return function () {
+    if (!active) {
+      blockMap.classList.remove("map--faded");
+      adForm.classList.remove("ad-form--disabled");
+      elementFieldsetsAdForm.forEach(function (element) {
+        element.disabled = false;
+      });
+      elementSelectsFilterForm.forEach(function (element) {
+        element.disabled = false;
+      });
+      fieldsetFilterForm.disabled = false;
+      active = true;
+    }
+  };
+};
+
+const fnActiveState = setActiveState();
+
+mainPin.addEventListener('mousedown', function (evt) {
+  if (evt.button === 0) {
+    evt.preventDefault();
+    setMoveAddressFieldAd();
+    fnActiveState();
+  }
+});
+
+mainPin.addEventListener('keydown', function (evt) {
+  if (evt.key === 'Enter') {
+    fnActiveState();
+  }
+});
+
+fieldRoom.addEventListener("change", function () {
+  if (fieldCapacity.value !== fieldRoom.value) {
+    fieldRoom.setCustomValidity("Укажите соответствующее количестве");
+  } else {
+    fieldRoom.setCustomValidity("");
+  }
+  fieldRoom.reportValidity();
+});
+
+setDisabledState();
